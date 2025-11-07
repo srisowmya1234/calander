3310036c366263541ef4637f1d90ec41861e4812
srisowmya1234 <137055248+srisowmya1234@users.noreply.github.com>
2025-11-08 00:14:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Calendar from "./components/Calendar";
+
+function App() {
+  return (
+    <div style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
